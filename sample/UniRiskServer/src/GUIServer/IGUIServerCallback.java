b466493d8423b9cf920f0d60a6078b8812a739af
bosinoliver <oliver.bosin@unibw.de>
2018-10-30 20:53:52 +0100

    Schnittstellenbeschreibung: IGUIServerCallback, ISpielkontrolle

--- sample/UniRiskServer/src/GUIServer/IGUIServerCallback.java
+++ sample/UniRiskServer/src/GUIServer/IGUIServerCallback.java
@@ -1,5 +1,10 @@
 package GUIServer;
 
 public interface IGUIServerCallback {
-    boolean update();
+
+    /**
+     * Diese Methode wird von der Komponente "Spiellogik" aufgerufen, wenn relevante Spieldaten verändert wurden.
+     * Bei Aufruf holt sich die Komponente "GUIServer" die aktuellen Spieldaten und aktualisiert die Anzeige.
+     */
+    void update();
 }
