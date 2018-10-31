a92f802ae96167fcb3f9b5261d51b4375632531c
DE92 <43740213+DE92@users.noreply.github.com>
2018-10-31 08:39:55 +0100

    ArchDok neues PDF, ISpieldaten erster Entwurf

--- sample/UniRiskServer/src/Spieldaten/ISpieldaten.java
+++ sample/UniRiskServer/src/Spieldaten/ISpieldaten.java
@@ -2,4 +2,33 @@ package Spieldaten;
 
 public interface ISpieldaten {
 
+    /**
+     * Diese Methode soll die Nachbargebäude zurückgeben.
+     *
+     * @param gebaeude
+     */
+    void Nachbargebaeude(String gebaeude);
+
+
+    /**
+     * Diese Methode soll die Anzahl der Ersties auf den jeweiligen Gebäuden zurückgeben.
+     * @param gebaeude
+     */
+    void AnzahlErstiesGebaeude(String gebaeude);
+
+
+    /**
+     * Diese Methode soll die Anzahl und den Typ der jeweiligen eigenen Karten zurückgeben.
+     * @param spieler
+     */
+    void TypderEigenenKarten(String spieler);
+
+
+    /**
+     * Diese Methode soll die Anzahl der besetzten Gebäude eines Spielers zurückgeben.
+     * @param spieler
+     */
+    void AnzahlLaenderSpieler(String spieler);
+
+
 }
