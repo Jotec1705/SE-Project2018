9c8630b4c0dc251db2535ff8eec27d80380f4c62
siefert1 <alicia.siefert@gmx.de>
2018-11-03 14:42:30 +0100

    auch meine Beschreibungen sind jetzt mit drin. Signed-off-by: siefert1 <alicia.siefert@gmx.de>

--- sample/UniRiskClient/src/GUIClient/IGUIClientCallback.java
+++ sample/UniRiskClient/src/GUIClient/IGUIClientCallback.java
@@ -2,14 +2,43 @@ package GUIClient;
 
 public interface IGUIClientCallback {
 
+    /**
+     * Diese Methode wird aufgerufen, wenn die Karte verändert wurde
+     * und der Client dies angezeigt bekommen sollte.
+     * Beim Aufruf holt sich die Komponente "GUIClient" die aktuellen Daten der Karte und
+     * aktualisiert diese Anzeige.
+     * @return zeigt, ob die Aktualisierung erfolgreich war.
+     */
     boolean aktualisierenKarte();
 
+    /**
+     * Diese Methode wird aufgerufen, wenn die Lobby verändert wurde.
+     * Beim Aufruf der Methode holt sich die Komponente "GUIClient" die aktuellen Daten der Lobby
+     * und aktualisiert die Anzeige.
+     * @return zeigt, ob die Aktualisierung erfolgreich war.
+     */
     boolean aktualisierenLobby();
 
+    /**
+     * Diese Methode beendet das Spiel und schließt die Karte sowie die Lobby beim Client.
+     * @return zeigt, ob das Spiel erfolgreich beendet wurde.
+     */
     boolean spielBeendet();
 
+    /**
+     * Diese Methode wird aufgerufen, wenn ein Spieler seinen Zug beendet hat. Dann wird der nächste Spieler ausgewählt.
+     * @param nameSpieler entspricht dem Namen des Spielers.
+     * @return zeigt, ob die Methode erfolgreich ausgeführt wurde.
+     */
     boolean zugZuteilung(String nameSpieler);
 
+    /**
+     * Diese Methode wird aufgerufen, wenn ein Spieler angegriffen wird und sich verteidigen muss.
+     * Danach wird die Methode wuefeln aufgerufen.
+     * @param nameSpieler entspricht dem Namen des Spielers.
+     * @param verteidigerGeb entspricht der ID des angegriffenen Gebäudes des Verteidigers.
+     * @return zeigt, ob die Methode erfolgreich ausgeführt wurde.
+     */
     boolean angriffAbwehren(String nameSpieler, Integer verteidigerGeb);
 
 }
--- sample/UniRiskClient/src/KommunikationClient/IClientKommunikation.java
+++ sample/UniRiskClient/src/KommunikationClient/IClientKommunikation.java
@@ -1,6 +1,86 @@
 package KommunikationClient;
 
+import GUIClient.IGUIClientCallback;
+
 public interface IClientKommunikation {
 
+    /**
+     * Mit dieser Methode kann man sich anmelden. Man gibt einen Namen und ein Passwort ein.
+     * @param nameSpieler ist der Name des Spielers.
+     * @param passwort ist das Passwort dieses Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean spielerAnmelden(String nameSpieler, String passwort);
 
+    /**
+     * Mit dieser Methode kann ein Spieler aus dem Spiel aussteigen.
+     * Sie liefert "true" zurück, nachdem der Button "Aussteigen" in der Lobby betätigt wurde.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean spielerAusgestiegen(String nameSpieler);
+
+    /**
+     * Mit dieser Methode kann sich ein Spieler bereit melden für ein Spiel.
+     * Sie liefert "true" zurück, nachdem der Button "bereit" in der Lobby betätogt wurde.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean spielerBereitMelden(String nameSpieler);
+
+    /**
+     * Mit dieser Methode kann man die Anzahl der Ersties auf einem Gebäude erhöhen.
+     * Nach klicken auf ein Gebäude(label) wird die Anzahl um eins erhöht.
+     * @param gebaeude die ID des Gebäudes.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean erstiesAnzahlErhoehen(Integer gebaeude, String nameSpieler);
+
+    /**
+     * Mit dieser Methode kann man einen Angriff starten.
+     * Sie wird durch klicken des Buttons "Angreifen" aufgerufen und dann kann man Angeben von welchem eigenen Gebäude
+     * man mit wie vielen Ersties welches gegnerische Gebäude angreifen will.
+     * @param gebaeudeUrsprung die ID des eigenen Gebäudes, mit dem man angreifen möchte.
+     * @param anzahlUrsprung Anzahl der Ersties, mit denen man angreifen will.
+     * @param gebaeudeZiel die ID des gegnerischen Gebäudes, was man angreifen will.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean angriffVonNach(Integer gebaeudeUrsprung, Integer anzahlUrsprung, String gebaeudeZiel, String nameSpieler);
+
+    /**
+     * Mit dieser Methode kann man Ersties verschieben.
+     * Sie wird durch klicken des Buttons "Verschieben" aufgerufen und dann kann man Angeben von wo nach wo man wie
+     * viele Ersties verschieben will.
+     * @param gebaeudeUrsprung die ID des Gebäudes, von dem man Ersties wegverschieben will.
+     * @param anzahlUrsprung die Anzahl der Ersties, die man verschieben will.
+     * @param gebaeudeZiel die ID des Gebäudes, welches verstärkt werden soll.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean versetzenVonNach(String gebaeudeUrsprung, Integer anzahlUrsprung, String gebaeudeZiel, String nameSpieler);
+
+    /**
+     * Diese Methode beendet den Zug und wird durch den Button "Zug beenden" aufgerufen.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean zugBeendet(String nameSpieler);
+
+    /**
+     * Diese Methode wird durch den Button "würfeln" aufgerufen und gibt somit an, das von diesem einen Spieler
+     * gewürfelt wurde.
+     * @param nameSpieler ist der Name des Spielers.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean gewuerfelt(String nameSpieler);
+
+    /**
+     * Diese Methode fügt den Beobachter GUIClient hinzu, um das Observerpattern zu realisieren.
+     * Die Rückmeldungen, die dann über diesen "Beobachter Kanal" kommen werden in IGUIClientCallback spezifiziert.
+     * @param beobachter ist der Observer, der hinzugefügt wird.
+     * @return gibt zurück, ob die Methode erfolgreich war.
+     */
+    boolean beobachterHinzufuegen(IGUIClientCallback beobachter);
 }
