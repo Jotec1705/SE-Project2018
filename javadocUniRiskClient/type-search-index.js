0cfce302b411237d30af2349e2cdb7990a5962d6
Johannes <johannestechel@gmail.com>
2018-11-06 22:16:18 +0100

    Javadoc genereriert für Client weils bei David nicht funktioniert hat

---
+++ javadocUniRiskClient/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"GUIClient","l":"GUIClient"},{"p":"KommunikationClient","l":"IClientKommunikation"},{"p":"GUIClient","l":"IGUIClientCallback"},{"p":"KommunikationClient","l":"KommunikationClient"}]
