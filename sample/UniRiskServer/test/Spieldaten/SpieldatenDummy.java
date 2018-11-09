fd5810a245dec1148d320c58840902a122b7d71d
robbie489 <r.lauenroth@gmx.de>
2018-11-09 22:49:26 +0100

    SpieldatenDummy hinzugefügt

---
+++ sample/UniRiskServer/test/Spieldaten/SpieldatenDummy.java
@@ -0,0 +1,14 @@
+package Spieldaten;
+
+public class SpieldatenDummy {
+
+    public String nameSpieler = "Horst";
+
+    public Integer gebaeude = 1;
+
+    public Integer[] nachbarGebaeude = {2,3,12,13};
+
+
+
+
+}
