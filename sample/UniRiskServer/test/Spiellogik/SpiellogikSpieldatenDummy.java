d465e2e2a7aa36da06143bf8e418eb3a96452f20
Johannes <johannestechel@gmail.com>
2018-11-13 18:11:20 +0100

    SpieldatenDummy nochmal in Absprache mit Alicia ergänzt

--- sample/UniRiskServer/test/Spiellogik/SpiellogikSpieldatenDummy.java
+++ sample/UniRiskServer/test/Spiellogik/SpiellogikSpieldatenDummy.java
@@ -6,6 +6,8 @@ import Spieldaten.Zustand;
 
 public class SpiellogikSpieldatenDummy {
 
+    //##############################Ab hier die Methoden der ISpieldaten#################################################
+
     /*Valide Parameterwerte "Horst", "M@rianne"*/
     Integer anzahlGebaeudeSpieler(String nameSpieler){
 
@@ -227,7 +229,7 @@ public class SpiellogikSpieldatenDummy {
         return aktuellerZustand == Zustand.Aktiv;
     }
 
-    //##############################Ab hier die Methoden der Spiellogik#################################################
+    //##############################Ab hier die Methoden der ISpiellogik#################################################
 
     /*Die Methode ist bereits durch den Spieldatenteil implementiert
     boolean spielerAnmelden(String nameSpieler, String passwort);
@@ -270,6 +272,113 @@ public class SpiellogikSpieldatenDummy {
     boolean gewuerfelt(String nameSpieler){
         return nameSpieler == "M@rianne";
     }
+
+    //##################################Ab  hier die Methoden IAnzeigedaten###########################
+
+
+    String aktuellePhase(){
+        return "PhaseII";
+    }
+
+    /*Valide Parameterwerte "Horst" | "M@rianne" | "P3t3r" */
+    Integer[] anzahlBonuskarten(String nameSpieler) {
+        Integer[] anzahl = {0, 0, 0};
+        switch (nameSpieler){
+            case "Horst":
+                anzahl[1] = 2;
+                break;
+            case "M@rianne":
+                anzahl[0] = 1;
+                anzahl[2] = 1;
+                break;
+            case "P3t3r":
+                anzahl[0] = 3;
+                break;
+            default:
+                break;
+        }
+        return anzahl;
+    }
+
+    /*Valide Parameterwerte "M@rianne"*/
+    Integer anzahlZuVerteilendeErsties(String nameSpieler) {
+        Integer anzahl = 0;
+        if (nameSpieler == "M@rianne")
+            anzahl = 3;
+        return anzahl;
+    }
+
+    Integer[] anzahlErstiesAufGebaeude() {
+        Integer[] ersties = {5, 1, 1, 2, 3};
+        return ersties;
+    }
+
+    /*Valide Parameter "Horst" | "P3t3r" | "M@rianne"*/
+    Integer[] eigeneGebaeude(String nameSpieler) {
+        switch (nameSpieler){
+            case "Horst":
+                Integer[] gebaeude = {3, 8, 17, 21};
+                return gebaeude;
+            case "P3t3r":
+                Integer[] gebaeude2 = {2, 10, 19, 27};
+                return gebaeude2;
+            case "M@rianne":
+                Integer[] gebaeude3 = {1, 6, 22, 30};
+                return gebaeude3;
+            default:
+                break;
+        }
+        return new Integer[0];
+    }
+
+    /*Valide Parameter 17, "Horst"*/
+    Integer[] angreifbareNachbarGebaeude(Integer gebaeude, String nameSpieler) {
+        if(gebaeude == 17 && nameSpieler == "Horst"){
+            Integer[] nachbar = {10, 18};
+            return nachbar;
+        }
+        return new Integer[0];
+    }
+
+    /*Valide Parameter 4 | 5 | 11 | 12 | 13 | 15, "Bob"*/
+    Integer[] eigeneNachbarGebaeude(Integer gebaeude, String nameSpieler) {
+        Integer[] eigene = {null};
+        if(nameSpieler == "Bob")
+            switch (gebaeude) {
+                case 4:
+                    eigene[0] = 5;
+                    break;
+                case 5:
+                    eigene[0] = 4;
+                    break;
+                case 11:
+                    eigene[0] = 12;
+                    break;
+                case 12:
+                    eigene[0] = 11;
+                    eigene[1] = 13;
+                    eigene[2] = 15;
+                    break;
+                case 13:
+                    eigene[0] = 12;
+                    eigene[1] = 15;
+                    break;
+                case 15:
+                    eigene[0] = 12;
+                    eigene[1] = 13;
+                    break;
+                default:
+                    break;
+            }
+        return eigene;
+    }
+
+
+    Integer[] farbeSpieler() {
+        Integer[] farbe = {3, 8, 10};
+        return farbe;
+    }
+
 
 
 }
