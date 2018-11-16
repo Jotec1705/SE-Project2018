f3439e5ba52105f961956ae7bc15b1270464fe6c
robbie489 <r.lauenroth@gmx.de>
2018-11-16 20:36:48 +0100

    Änderungen an Methode und Classes eingebunden

--- sample/UniRiskServer/src/Spieldaten/Spieldaten.java
+++ sample/UniRiskServer/src/Spieldaten/Spieldaten.java
@@ -7,12 +7,6 @@ public class Spieldaten implements ISpieldaten{
 
    // Gebaeude geb = new Gebaeude("test",1,1,"tom");
    // Gebaeude geb2 = new Gebaeude("test2",21,2,"tim");
-
-
-
-
-
-
     @Override
     public Integer anzahlGebaeudeSpieler(String nameSpieler) {
 
@@ -35,73 +29,73 @@ public class Spieldaten implements ISpieldaten{
         //Abbruchkriterien
         Integer[] nachbarGebaeude;
         switch(gebaeude){
-            case 1: nachbarGebaeude = {2,3,12,13};
+            case 1: nachbarGebaeude = new Integer[]{2, 3, 12, 13};
                 break;
-            case 2: nachbarGebaeude = {1,3,16,,22,27,33};
+            case 2: nachbarGebaeude = new Integer[]{1,3,16,22,27,33};
                 break;
-            case 3: nachbarGebaeude = {1,2,4,6,7,13};
+            case 3: nachbarGebaeude = new Integer[]{1,2,4,6,7,13};
                 break;
-            case 4: nachbarGebaeude = {3,5,6};
+            case 4: nachbarGebaeude = new Integer[]{3,5,6};
                 break;
-            case 5: nachbarGebaeude = {4,6};
+            case 5: nachbarGebaeude = new Integer[] {4,6};
                 break;
-            case 6: nachbarGebaeude = {3,4,5,7,9};
+            case 6: nachbarGebaeude = new Integer[]{3,4,5,7,9};
                 break;
-            case 7: nachbarGebaeude = {3,6,8,9};
+            case 7: nachbarGebaeude = new Integer[]{3,6,8,9};
                 break;
-            case 8: nachbarGebaeude = {7,9};
+            case 8: nachbarGebaeude = new Integer[]{7,9};
                 break;
-            case 9: nachbarGebaeude = {6,7,8,16};
+            case 9: nachbarGebaeude = new Integer[]{6,7,8,16};
                 break;
-            case 10: nachbarGebaeude = {11,12,17,18};
+            case 10: nachbarGebaeude = new Integer[]{11,12,17,18};
                 break;
-            case 11: nachbarGebaeude = {10,12};
+            case 11: nachbarGebaeude = new Integer[]{10,12};
                 break;
-            case 12: nachbarGebaeude = {1,10,11,13,15};
+            case 12: nachbarGebaeude = new Integer[]{1,10,11,13,15};
                 break;
-            case 13: nachbarGebaeude = {1,3,12,14,15};
+            case 13: nachbarGebaeude = new Integer[]{1,3,12,14,15};
                 break;
-            case 14: nachbarGebaeude = {13,15};
+            case 14: nachbarGebaeude = new Integer[]{13,15};
                 break;
-            case 15: nachbarGebaeude = {12,13,14,16};
+            case 15: nachbarGebaeude = new Integer[]{12,13,14,16};
                 break;
-            case 16: nachbarGebaeude = {2,9,15,22,27,33};
+            case 16: nachbarGebaeude = new Integer[]{2,9,15,22,27,33};
                 break;
-            case 17: nachbarGebaeude = {10,18};
+            case 17: nachbarGebaeude = new Integer[]{10,18};
                 break;
-            case 18: nachbarGebaeude = {10,17,19,20};
+            case 18: nachbarGebaeude = new Integer[]{10,17,19,20};
                 break;
-            case 19: nachbarGebaeude = {18,20,21};
+            case 19: nachbarGebaeude = new Integer[]{18,20,21};
                 break;
-            case 20: nachbarGebaeude = {18,19,21,25,26};
+            case 20: nachbarGebaeude = new Integer[]{18,19,21,25,26};
                 break;
-            case 21: nachbarGebaeude = {19,20,22};
+            case 21: nachbarGebaeude = new Integer[]{19,20,22};
                 break;
-            case 22: nachbarGebaeude = {2,16,21,27,33};
+            case 22: nachbarGebaeude = new Integer[]{2,16,21,27,33};
                 break;
-            case 23: nachbarGebaeude = {24,27,28};
+            case 23: nachbarGebaeude = new Integer[]{24,27,28};
                 break;
-            case 24: nachbarGebaeude = {23,25,26,29};
+            case 24: nachbarGebaeude = new Integer[]{23,25,26,29};
                 break;
-            case 25: nachbarGebaeude = {20,24,26};
+            case 25: nachbarGebaeude = new Integer[]{20,24,26};
                 break;
-            case 26: nachbarGebaeude = {20,24,25,29,30};
+            case 26: nachbarGebaeude = new Integer[]{20,24,25,29,30};
                 break;
-            case 27: nachbarGebaeude = {2,16,22,23,28,30};
+            case 27: nachbarGebaeude = new Integer[]{2,16,22,23,28,30};
                 break;
-            case 28: nachbarGebaeude = {23,27,29};
+            case 28: nachbarGebaeude = new Integer[]{23,27,29};
                 break;
-            case 29: nachbarGebaeude = {24,26,28,30};
+            case 29: nachbarGebaeude = new Integer[]{24,26,28,30};
                 break;
-            case 30: nachbarGebaeude = {26,29};
+            case 30: nachbarGebaeude = new Integer[]{26,29};
                 break;
-            case 31: nachbarGebaeude = {32,33};
+            case 31: nachbarGebaeude = new Integer[]{32,33};
                 break;
-            case 32: nachbarGebaeude = {31,33};
+            case 32: nachbarGebaeude = new Integer[]{31,33};
                 break;
-            case 33: nachbarGebaeude = {2,16,22,27,31,32};
+            case 33: nachbarGebaeude = new Integer[]{2,16,22,27,31,32};
                 break;
-            default: nachbarGebaeude = {0};
+            default: nachbarGebaeude = new Integer[]{0};
         }
         return nachbarGebaeude;
     }
@@ -128,7 +122,7 @@ public class Spieldaten implements ISpieldaten{
     }
 
     @Override
-    public Spieldaten.Missionskarte missionskarteSpieler(String nameSpieler) {
+    public Missionskarte missionskarteSpieler(String nameSpieler) {
         return null;
     }
 
@@ -163,22 +157,22 @@ public class Spieldaten implements ISpieldaten{
     }
 
     @Override
-    public Spieldaten.Zustand spielZustandHolen() {
+    public Zustand spielZustandHolen() {
         return null;
     }
 
     @Override
-    public boolean spielZustandSetzen(Spieldaten.Zustand aktuellerZustand) {
+    public boolean spielZustandSetzen(Zustand aktuellerZustand) {
         return false;
     }
 
     @Override
-    public boolean anzahlDerBonuskartenAnpassen(String nameSpieler, Spieldaten.Bonuskarte typBonuskarte, Integer anzahlBonuskarten) {
+    public boolean anzahlDerBonuskartenAnpassen(String nameSpieler, Bonuskarte typBonuskarte, Integer anzahlBonuskarten) {
         return false;
     }
 
     @Override
-    public Integer anzahlDerBonuskarten(String nameSpieler, Spieldaten.Bonuskarte typBonuskarte) {
+    public Integer anzahlDerBonuskarten(String nameSpieler, Bonuskarte typBonuskarte) {
         return null;
     }
 }
