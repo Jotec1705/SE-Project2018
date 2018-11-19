24fccdfeb87fee247792b618c867e4a42932b7eb
Johannes <johannestechel@gmail.com>
2018-11-19 15:46:12 +0100

    Änderungen aus der Besprechung

--- sample/UniRiskServer/src/Spieldaten/Gebaeude.java
+++ sample/UniRiskServer/src/Spieldaten/Gebaeude.java
@@ -4,9 +4,10 @@ package Spieldaten;
 
 public class Gebaeude {
     public final String Name;
-    public final Integer ID;
+    public final Integer ID;//ID raus und als index vom array. (Erstes Element im array bleibt null)
     public Integer anzahlErsties;
     public String Besitzer;
+    //Array mit nachbargebäuden
 
     public Gebaeude(String Name, Integer ID, Integer anzahlErsties, String Besitzer){
         this.Name = Name;
