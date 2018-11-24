bd5d24def578dd824c41c27df23cdd1fba653e07
robbie489 <r.lauenroth@gmx.de>
2018-11-24 22:54:38 +0100

    Spieldaten erweitert

--- sample/UniRiskServer/src/Spieldaten/Spieldaten.java
+++ sample/UniRiskServer/src/Spieldaten/Spieldaten.java
@@ -86,9 +86,35 @@ public class Spieldaten implements ISpieldaten, IAnzeigedaten{
 
         if(spielerAnzahl >5 || spielerAnzahl <3){
             return false;  //hier vieleicht noch nacch NaN testen?
+
+
+
         }
         if(spielerAnzahl>=3 && spielerAnzahl<=5) {
 
+            Integer[] zufallsArray3 = { 1,1,1,1,1,1,1,1,1,1,1,
+                                        2,2,2,2,2,2,2,2,2,2,2,
+                                        3,3,3,3,3,3,3,3,3,3,3 };
+
+            Integer[] zufallsArray4 = { 1,1,1,1,1,1,1,1,1,
+                                        2,2,2,2,2,2,2,2,
+                                        3,3,3,3,3,3,3,3,
+                                        4,4,4,4,4,4,4,4 };
+
+            Integer[] zufallsArray5 = { 1,1,1,1,1,1,1,1,1,
+                                        2,2,2,2,2,2,2,2,2,
+                                        3,3,3,3,3,3,3,3,3,
+                                        4,4,4,4,4,4,
+                                        5,5,5,5,5,5 };
+
+            Integer[] zufallsArray= new Integer[33];
+
+
+
+            for(int i = 0;i < 33 ;i++){
+
+
+            }
         // Array vom Typ Gebaude wird angelegt
 
 
@@ -97,41 +123,41 @@ public class Spieldaten implements ISpieldaten, IAnzeigedaten{
         gebaeudeArr[0] = null;
         // Nun werden alle Gebäude angelegt
         //Hier könnte schon die Zuteilung der Gebäude an die Spieler statt finden
-/*
-        gebaeudeArr[1] = new Gebaeude("Hotel Alpenblick",1,1,null, Fachbereiche.A ,new Integer[] {2, 3, 12, 13});
-        gebaeudeArr[2] = new Gebaeude("Parkplatz Ost",2,1,null,new Integer[] {1,3,16,22,27,33});
-        gebaeudeArr[3] = new Gebaeude("Freizeit-Area",3,1,null,new Integer[] {1,2,4,6,7,13});
-        gebaeudeArr[4] = new Gebaeude("Sternbau",4,1,null,new Integer[] {3,5,6});
-        gebaeudeArr[5] = new Gebaeude("ET_Bistro",5,1,null,new Integer[] {4,6});
-        gebaeudeArr[6] = new Gebaeude("Eifriger",6,1,null,new Integer[] {3,4,5,7,9});
-        gebaeudeArr[7] = new Gebaeude("Stab",7,1,null,new Integer[] {3,6,8,9});
-        gebaeudeArr[8] = new Gebaeude("Mensa",8,1,null,new Integer[] {7,9});
-        gebaeudeArr[9] = new Gebaeude("Code",9,1,null,new Integer[] {6,7,8,16});
-        gebaeudeArr[10] = new Gebaeude("Teststrecke",10,1,null,new Integer[] {11,12,17,18});
-        gebaeudeArr[11] = new Gebaeude("BND_Baracke",11,1,null,new Integer[] {10,12});
-        gebaeudeArr[12] = new Gebaeude("Fuhrpark",12,1,null,new Integer[] {1,10,11,13,15});
-        gebaeudeArr[13] = new Gebaeude("Rocket",13,1,null,new Integer[] {1,3,12,14,15});
-        gebaeudeArr[14] = new Gebaeude("OPZ",14,1,null,new Integer[] {13,15});
-        gebaeudeArr[15] = new Gebaeude("Sporthalle",15,1,null,new Integer[] {12,13,14,16});
-        gebaeudeArr[16] = new Gebaeude("Lerntempel",16,1,null,new Integer[] {2,9,15,22,27,33});
-        gebaeudeArr[17] = new Gebaeude("Ackerbau",17,1,null,new Integer[] {10,18});
-        gebaeudeArr[18] = new Gebaeude("Athene Racing",18,1,null,new Integer[] {10,17,19,20});
-        gebaeudeArr[19] = new Gebaeude("Prüfungsamt",19,1,null,new Integer[] {18,20,21});
-        gebaeudeArr[20] = new Gebaeude("Mucki-Bude",20,1,null,new Integer[] {18,19,21,25,26});
-        gebaeudeArr[21] = new Gebaeude("Tempel de Wissens",21,1,null,new Integer[] {19,20,22});
-        gebaeudeArr[22] = new Gebaeude("Sprachenzentrum",22,1,null,new Integer[] {2,16,21,27,33});
-        gebaeudeArr[23] = new Gebaeude("Driving Range",23,1,null,new Integer[] {24,27,28});
-        gebaeudeArr[24] = new Gebaeude("Schicki-Micki-Neubau",24,1,null,new Integer[] {23,25,26,29});
-        gebaeudeArr[25] = new Gebaeude("Flight",25,1,null,new Integer[] {20,24,26});
-        gebaeudeArr[26] = new Gebaeude("RZ & PAUS",26,1,null,new Integer[] {20,24,25,29,30});
-        gebaeudeArr[27] = new Gebaeude("Radarhalle",27,1,null,new Integer[] {2,16,22,23,28,30});
-        gebaeudeArr[28] = new Gebaeude("Sportplatz",28,1,null,new Integer[] {23,27,29});
-        gebaeudeArr[29] = new Gebaeude("Casino",29,1,null,new Integer[] {24,26,28,30});
-        gebaeudeArr[30] = new Gebaeude("Wäsche & Lager",30,1,null,new Integer[] {26,29});
-        gebaeudeArr[31] = new Gebaeude("Fliegerpuff",31,1,null,new Integer[] {32,33});
-        gebaeudeArr[32] = new Gebaeude("Hirschkäfer",32,1,null,new Integer[] {31,33});
-        gebaeudeArr[33] = new Gebaeude("Wache",33,1,null,new Integer[] {2,16,22,27,31,32});
-        */
+
+        gebaeudeArr[1] = new Gebaeude("Hotel Alpenblick",1,1,null, Fachbereiche.B,new Integer[] {2, 3, 12, 13});
+        gebaeudeArr[2] = new Gebaeude("Parkplatz Ost",2,1,null, Fachbereiche.B,new Integer[] {1,3,16,22,27,33});
+        gebaeudeArr[3] = new Gebaeude("Freizeit-Area",3,1,null, Fachbereiche.B,new Integer[] {1,2,4,6,7,13});
+        gebaeudeArr[4] = new Gebaeude("Sternbau",4,1,null, Fachbereiche.B,new Integer[] {3,5,6});
+        gebaeudeArr[5] = new Gebaeude("ET_Bistro",5,1,null, Fachbereiche.B,new Integer[] {4,6});
+        gebaeudeArr[6] = new Gebaeude("Eifriger",6,1,null, Fachbereiche.B,new Integer[] {3,4,5,7,9});
+        gebaeudeArr[7] = new Gebaeude("Stab",7,1,null, Fachbereiche.B,new Integer[] {3,6,8,9});
+        gebaeudeArr[8] = new Gebaeude("Mensa",8,1,null, Fachbereiche.B,new Integer[] {7,9});
+        gebaeudeArr[9] = new Gebaeude("Code",9,1,null, Fachbereiche.B,new Integer[] {6,7,8,16});
+        gebaeudeArr[10] = new Gebaeude("Teststrecke",10,1,null, Fachbereiche.A,new Integer[] {11,12,17,18});
+        gebaeudeArr[11] = new Gebaeude("BND_Baracke",11,1,null, Fachbereiche.A,new Integer[] {10,12});
+        gebaeudeArr[12] = new Gebaeude("Fuhrpark",12,1,null, Fachbereiche.A,new Integer[] {1,10,11,13,15});
+        gebaeudeArr[13] = new Gebaeude("Rocket",13,1,null, Fachbereiche.A,new Integer[] {1,3,12,14,15});
+        gebaeudeArr[14] = new Gebaeude("OPZ",14,1,null, Fachbereiche.A,new Integer[] {13,15});
+        gebaeudeArr[15] = new Gebaeude("Sporthalle",15,1,null, Fachbereiche.A,new Integer[] {12,13,14,16});
+        gebaeudeArr[16] = new Gebaeude("Lerntempel",16,1,null, Fachbereiche.A,new Integer[] {2,9,15,22,27,33});
+        gebaeudeArr[17] = new Gebaeude("Ackerbau",17,1,null, Fachbereiche.D,new Integer[] {10,18});
+        gebaeudeArr[18] = new Gebaeude("Athene Racing",18,1,null, Fachbereiche.D,new Integer[] {10,17,19,20});
+        gebaeudeArr[19] = new Gebaeude("Prüfungsamt",19,1,null, Fachbereiche.D,new Integer[] {18,20,21});
+        gebaeudeArr[20] = new Gebaeude("Mucki-Bude",20,1,null, Fachbereiche.D,new Integer[] {18,19,21,25,26});
+        gebaeudeArr[21] = new Gebaeude("Tempel de Wissens",21,1,null, Fachbereiche.D,new Integer[] {19,20,22});
+        gebaeudeArr[22] = new Gebaeude("Sprachenzentrum",22,1,null, Fachbereiche.D,new Integer[] {2,16,21,27,33});
+        gebaeudeArr[23] = new Gebaeude("Driving Range",23,1,null, Fachbereiche.C,new Integer[] {24,27,28});
+        gebaeudeArr[24] = new Gebaeude("Schicki-Micki-Neubau",24,1,null, Fachbereiche.C,new Integer[] {23,25,26,29});
+        gebaeudeArr[25] = new Gebaeude("Flight",25,1,null, Fachbereiche.C,new Integer[] {20,24,26});
+        gebaeudeArr[26] = new Gebaeude("RZ & PAUS",26,1,null, Fachbereiche.C,new Integer[] {20,24,25,29,30});
+        gebaeudeArr[27] = new Gebaeude("Radarhalle",27,1,null, Fachbereiche.C,new Integer[] {2,16,22,23,28,30});
+        gebaeudeArr[28] = new Gebaeude("Sportplatz",28,1,null, Fachbereiche.C,new Integer[] {23,27,29});
+        gebaeudeArr[29] = new Gebaeude("Casino",29,1,null, Fachbereiche.C,new Integer[] {24,26,28,30});
+        gebaeudeArr[30] = new Gebaeude("Wäsche & Lager",30,1,null, Fachbereiche.C,new Integer[] {26,29});
+        gebaeudeArr[31] = new Gebaeude("Fliegerpuff",31,1,null, Fachbereiche.AERO,new Integer[] {32,33});
+        gebaeudeArr[32] = new Gebaeude("Hirschkäfer",32,1,null, Fachbereiche.AERO,new Integer[] {31,33});
+        gebaeudeArr[33] = new Gebaeude("Wache",33,1,null, Fachbereiche.AERO,new Integer[] {2,16,22,27,31,32});
+        
         return true;
         }
         return false;
@@ -282,5 +308,12 @@ public class Spieldaten implements ISpieldaten, IAnzeigedaten{
     @Override
     public Integer anzahlDerBonuskarten(String nameSpieler, Bonuskarte typBonuskarte) {
         return null;
+    }
+
+    /**
+     * Dies Methode dient dazu ein Array(Interger) zu mischen um die Karten später an die Spieler zu verteilen
+     */
+    public Integer[] arrayMischen(Integer[] zufallsArray ){
+        return new Integer[2];
     }
 }
